4ad83881fc9984970e810c6195005a65364799b4
HBinhCT <HBinhCT@users.noreply.github.com>
2021-07-07 16:29:50 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.8a61bc1ca289783feae8427b1b986c24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b59a8b26bc8dc8ec0e60f5ef3b2c500",
+    "url": "https://hbinhct.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "31f3f8981da92e0f9511",
+    "url": "https://hbinhct.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "a8f9cfb31798c2744716",
+    "url": "https://hbinhct.github.io/task-management-frontend/static/js/2.65b67ecb.chunk.js"
+  },
+  {
+    "revision": "31f3f8981da92e0f9511",
+    "url": "https://hbinhct.github.io/task-management-frontend/static/js/main.56badccd.chunk.js"
+  },
+  {
+    "revision": "a098e1eead9762b0e4a2",
+    "url": "https://hbinhct.github.io/task-management-frontend/static/js/runtime~main.f056b3e7.js"
+  }
+]);
